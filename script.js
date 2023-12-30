144e5f4df68da019b98816548167a48f778652d8
Roshan Bhattarai <roshan.bhattarai@proroshan.com>
2023-12-30 22:17:15 +1100

    working on js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const ApiUrl = 'https://v6.exchangerate-api.com/v6/b951616ff58282446c085090/latest/USD';
 const currencyContainer = document.querySelector('#currency-one')
 const exchangeCurrency = document.querySelector('#currency-two')
-console.log(currencyContainer);
+const updateTimeElm = document.querySelector('.updateTime');
+const amountOne = document.querySelector('#amount_one')
+const currencyDiv = document.querySelectorAll('.currency')
 
 
+
+
+const getCode = () =>{
+
+
+    currencyDiv.forEach((elm)=>{
+        
+        elm.addEventListener('change', (e)=>{
+
+            console.log(e.target);
+
+        })
+    })
+}
+
+getCode()
 
 
 const displayData = (data)=>{
@@ -11,8 +29,11 @@ const displayData = (data)=>{
     const {conversion_rates: CR, time_last_update_utc: updateTime } = data;
     let d = new Date(updateTime)
     let sydneyTime = d.toLocaleString(undefined, {timeZone: 'Australia/Sydney'})
+    updateTimeElm.innerHTML = `Last Updated: ${sydneyTime}`
     let str = ''
     let str2 = ''
+
+    
 
     for(let elm in CR){
         // console.log(elm);
