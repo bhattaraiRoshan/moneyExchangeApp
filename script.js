72b1a873f3497f39a51972474ff91e56146732b1
Roshan Bhattarai <roshan.bhattarai@proroshan.com>
2023-12-30 20:30:34 +1100

    woking on js function

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-ApiUrl = 'https://v6.exchangerate-api.com/v6/b951616ff58282446c085090/latest/USD'
+const ApiUrl = 'https://v6.exchangerate-api.com/v6/b951616ff58282446c085090/latest/USD';
+const currencyContainer = document.querySelector('#currency-one')
+const exchangeCurrency = document.querySelector('#currency-two')
+console.log(currencyContainer);
 
 
 
 
 const displayData = (data)=>{
-console.log(data);
+
     const {conversion_rates: CR, time_last_update_utc: updateTime } = data;
     let d = new Date(updateTime)
     let sydneyTime = d.toLocaleString(undefined, {timeZone: 'Australia/Sydney'})
+    let str = ''
+    let str2 = ''
+
+    for(let elm in CR){
+        // console.log(elm);
+
+        str += `
+        
+        <option value="${elm}">${elm}</option>
+        
+        
+        `;
+
+        str2 += `
+        <option value="${elm}">${elm}</option>
+        `
+    }
+
+    currencyContainer.innerHTML = str
+    exchangeCurrency.innerHTML = str2
     
     
 
